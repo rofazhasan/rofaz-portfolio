906a4d27454d0cade41040aa05c027906e67c0b2
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2023-10-24 23:17:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
-const data = {
-    name: "Md. Rofaz Hasan Rafiu",
-    university: "Rajshahi University of Engineering & Technology (RUET)",
-    expertise: "Backend Developer | Software Engineer | AI Enthusiast",
-    skills: ["C", "C++", "Python", "JavaScript"],
-    projects: ["Project 1", "Project 2", "Project 3"],
-    socialLinks: {
-        facebook: "https://www.facebook.com/yourfacebookprofile",
-        linkedin: "https://www.linkedin.com/in/yourlinkedinprofile",
-    }
-};
-
-document.getElementById("name").textContent = data.name;
-document.getElementById("university").textContent = data.university;
-document.getElementById("expertise").textContent = data.expertise;
-
-const skillsList = document.getElementById("skills-list");
-data.skills.forEach(skill => {
-    const skillItem = document.createElement("div");
-    skillItem.textContent = skill;
-    skillsList.appendChild(skillItem);
+document.getElementById("home-btn").addEventListener("click", () => {
+    document.getElementById("title").textContent = "Md Rofaz Hasan Rafiu";
+    document.getElementById("description").textContent = "Computer Science Student at RUET | Backend Developer | AI Enthusiast";
 });
 
-const projectsList = document.getElementById("projects-list");
-data.projects.forEach(project => {
-    const projectItem = document.createElement("div");
-    projectItem.textContent = project;
-    projectsList.appendChild(projectItem);
+document.getElementById("projects-btn").addEventListener("click", () => {
+    // Load and display projects dynamically
+    // You can fetch your projects from an API or use hardcoded data
+    // Update the content using JavaScript
 });
 
-const socialLinksDiv = document.getElementById("social-links");
-for (const [platform, link] of Object.entries(data.socialLinks)) {
-    const linkElement = document.createElement("a");
-    linkElement.href = link;
-    linkElement.target = "_blank";
-    linkElement.textContent = platform.charAt(0).toUpperCase() + platform.slice(1);
-    socialLinksDiv.appendChild(linkElement);
-}
-const sections = document.querySelectorAll("section");
-
-sections.forEach(section => {
-    const butterfly = document.createElement("div");
-    butterfly.classList.add("butterfly");
-    section.appendChild(butterfly);
+document.getElementById("contact-btn").addEventListener("click", () => {
+    document.getElementById("title").textContent = "Contact Me";
+    document.getElementById("description").textContent = "Email: mdrofaz@example.com | LinkedIn: linkedin.com/in/mdrofaz | Facebook: facebook.com/mdrofaz";
 });
 
-const buttons = document.querySelectorAll(".button");
+// Popup functionality
+const popup = document.getElementById("popup");
+const popupCloseBtn = document.getElementById("close-popup");
 
-buttons.forEach(button => {
-    button.addEventListener("mouseenter", () => {
-        const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-        button.style.backgroundColor = randomColor;
-    });
-
-    button.addEventListener("mouseleave", () => {
-        button.style.backgroundColor = "#3498db";
-    });
+popupCloseBtn.addEventListener("click", () => {
+    popup.style.display = "none";
 });
+
+// Show popup after a delay (for demonstration purposes)
+setTimeout(() => {
+    popup.style.display = "flex";
+}, 2000);
