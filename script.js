afc18cca4cb6bdc81c6614c2dd4fa1df6369fc89
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2023-10-24 23:21:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-document.getElementById("home-btn").addEventListener("click", () => {
-    document.getElementById("title").textContent = "Md Rofaz Hasan Rafiu";
-    document.getElementById("description").textContent = "Computer Science Student at RUET | Backend Developer | AI Enthusiast";
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    header.style.backgroundColor = "#007BFF"; // Change header color to blue
+
+    const dynamicElements = document.querySelectorAll(".dynamic");
+    dynamicElements.forEach((element) => {
+        element.style.color = "blue"; // Change color of dynamic elements to blue
+    });
+
+    const dynamicButtons = document.querySelectorAll(".dynamic-button");
+    dynamicButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            alert("Button clicked!"); // Add click event to dynamic buttons
+        });
+    });
 });
-
-document.getElementById("projects-btn").addEventListener("click", () => {
-    // Load and display projects dynamically
-    // You can fetch your projects from an API or use hardcoded data
-    // Update the content using JavaScript
-});
-
-document.getElementById("contact-btn").addEventListener("click", () => {
-    document.getElementById("title").textContent = "Contact Me";
-    document.getElementById("description").textContent = "Email: mdrofaz@example.com | LinkedIn: linkedin.com/in/mdrofaz | Facebook: facebook.com/mdrofaz";
-});
-
-// Popup functionality
-const popup = document.getElementById("popup");
-const popupCloseBtn = document.getElementById("close-popup");
-
-popupCloseBtn.addEventListener("click", () => {
-    popup.style.display = "none";
-});
-
-// Show popup after a delay (for demonstration purposes)
-setTimeout(() => {
-    popup.style.display = "flex";
-}, 2000);
