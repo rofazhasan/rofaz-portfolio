8b89e07a37fc97ffc95b8173a1ec23edd7e190ff
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2023-10-29 11:50:40 +0600

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,1 +1,9 @@
+function showDetails(projectId) {
+    var details = document.getElementById('details-' + projectId);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
 
